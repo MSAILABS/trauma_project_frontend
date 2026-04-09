e006af0e006d1b49d77e83ff77c8261f8015ecbd
Rehan <rehan.shah70@yahoo.com>
2026-04-09 11:19:44 +0500

    email updated

--- src/FirstPage.tsx
+++ src/FirstPage.tsx
@@ -1,58 +1,55 @@
 const FirstPage = () => {
-  return (
-    <main
-      style={{
-        minHeight: "100vh",
-        display: "grid",
-        gridTemplateRows: '100px auto 50px 50px',
-        alignItems: "center",
-        justifyContent: "center",
-        fontFamily: "sans-serif",
-        textAlign: "center",
-      }}
-    >
-      <h1
-        style={{
-          fontSize: "2rem",
-          letterSpacing: "1px",
-          textTransform: "uppercase",
-          margin: 0,
-        }}
-      >
-        TRAUMA VISUALIZATION DEMO
-      </h1>
+	return (
+		<main
+			style={{
+				minHeight: '100vh',
+				display: 'grid',
+				gridTemplateRows: '100px auto 50px 50px',
+				alignItems: 'center',
+				justifyContent: 'center',
+				fontFamily: 'sans-serif',
+				textAlign: 'center',
+			}}>
+			<h1
+				style={{
+					fontSize: '2rem',
+					letterSpacing: '1px',
+					textTransform: 'uppercase',
+					margin: 0,
+				}}>
+				TRAUMA VISUALIZATION DEMO
+			</h1>
 
-      <video
-        src={'graph_6_edited.mov'}
-        style={{
-          width: "80%",
-          maxWidth: "70vw",
-          borderRadius: "12px",
-          border: "2px solid #ccc",
-          objectFit: "cover",
-          margin: "0px auto",
-        }}
-        controls
-        loop
-      />
+			<video
+				src={'graph_6_edited.mov'}
+				style={{
+					width: '80%',
+					maxWidth: '70vw',
+					borderRadius: '12px',
+					border: '2px solid #ccc',
+					objectFit: 'cover',
+					margin: '0px auto',
+				}}
+				controls
+				loop
+			/>
 
-      <a href="mailto:marvasti.marvsai@gmail.com">
-        <button
-          style={{
-            padding: "12px 24px",
-            fontSize: "1rem",
-            cursor: "pointer",
-            borderRadius: "8px",
-            letterSpacing: "0.5px",
-          }}
-        >
-          Contact Us For Demo
-        </button>
-      </a>
+			<a href='mailto:info@msailabs.com'>
+				<button
+					style={{
+						padding: '12px 24px',
+						fontSize: '1rem',
+						cursor: 'pointer',
+						borderRadius: '8px',
+						letterSpacing: '0.5px',
+					}}>
+					Contact Us For Demo
+				</button>
+			</a>
 
-      <p>Email: marvasti.marvsai@gmail.com</p>
-    </main>
-  )
+			<p>Email: info@msailabs.com</p>
+		</main>
+	)
 }
 
 export default FirstPage
